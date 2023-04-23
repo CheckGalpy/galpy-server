87ce28850b56f4ee582a70be9c151614464532d8
j25nkh <j25nkh@gmail.com>
2023-04-23 23:16:47 +0900

    feat: 유저검색 / 팔로우정보 열람 엔드포인트 구현

--- routes/api/users/usersController.js
+++ routes/api/users/usersController.js
@@ -1,4 +1,49 @@
 const User = require("../../../models/User");
+const Follow = require("../../../models/Follow");
+
+exports.searchUser = async function (req, res, next) {
+  const { keyword, currentUserId } = req.query;
+
+  try {
+    const targetUsers = await User.find({
+      $or: [
+        { familyName: new RegExp(keyword, "i") },
+        { givenName: new RegExp(keyword, "i") },
+        { email: { $regex: new RegExp("^" + keyword, "i") } },
+        {
+          $expr: {
+            $regexMatch: {
+              input: { $concat: ["$familyName", "$givenName"] },
+              regex: new RegExp(keyword, "i"),
+            },
+          },
+        },
+      ],
+    }).exec();
+
+    const targetUsersFollowingStatus = {};
+
+    for (const targetUser of targetUsers) {
+      console.log(currentUserId, targetUser._id);
+
+      const existingFollow = await Follow.findOne({
+        followerId: currentUserId,
+        followeeId: targetUser._id,
+      });
+
+      console.log(existingFollow);
+
+      targetUsersFollowingStatus[targetUser._id] = !!existingFollow;
+    }
+
+    console.log(targetUsersFollowingStatus);
+    res.status(200).json({ targetUsers, targetUsersFollowingStatus });
+  } catch (error) {
+    res.status(500).json({
+      error: "서버 에러가 발생하여 사용자 검색에 실패하였습니다.",
+    });
+  }
+};
 
 exports.getUser = async function (req, res, next) {
   const { userId } = req.params;
--- routes/api/users/usersRouter.js
+++ routes/api/users/usersRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 const usersController = require("./usersController");
 
+router.get("/search", usersController.searchUser);
 router.get("/:userId", usersController.getUser);
 
 module.exports = router;
