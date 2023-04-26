05a2cc3d415b67f28d1fe44aa0952fd9c2c298cf
j25nkh <j25nkh@gmail.com>
2023-04-26 19:53:29 +0900

    feat: 책갈피 즐겨찾기 정보열람 엔드포인트 구현 및 분할 (모든계정 / 특정계정)

--- routes/api/collects/collectsController.js
+++ routes/api/collects/collectsController.js
@@ -62,3 +62,44 @@ exports.getCollectedBookmarks = async (req, res) => {
       .json({ error: "즐겨찾기된 책갈피 정보를 불러오는데 실패하였습니다" });
   }
 };
+
+exports.checkAnyCollectExists = async function (req, res, next) {
+  const { bookmarkId } = req.params;
+
+  try {
+    const collectExists = await Collect.findOne({
+      collectedBookmarkId: bookmarkId,
+    });
+
+    if (collectExists) {
+      res.status(200).json({ exists: true });
+    } else {
+      res.status(200).json({ exists: false });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "책갈피 즐겨찾기 상태를 불러오는데 실패하였습니다" });
+  }
+};
+
+exports.checkUserCollectExists = async function (req, res, next) {
+  const { userId, bookmarkId } = req.params;
+
+  try {
+    const collectExists = await Collect.findOne({
+      collectorId: userId,
+      collectedBookmarkId: bookmarkId,
+    });
+
+    if (collectExists) {
+      res.status(200).json({ exists: true });
+    } else {
+      res.status(200).json({ exists: false });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "해당 유저의 책갈피 즐겨찾기 상태를 불러오는데 실패하였습니다",
+    });
+  }
+};
--- routes/api/collects/collectsRouter.js
+++ routes/api/collects/collectsRouter.js
@@ -5,9 +5,10 @@ const collectsController = require("./collectsController");
 router.post("/collect", collectsController.collect);
 router.post("/discard", collectsController.discard);
 router.get("/:userId/collected", collectsController.getCollectedBookmarks);
+router.get("/:bookmarkId/exists", collectsController.checkAnyCollectExists);
 router.get(
   "/:userId/:bookmarkId/exists",
-  collectsController.checkCollectExists,
+  collectsController.checkUserCollectExists,
 );
 
 module.exports = router;
